eecc9e42ee8340cf1e72f360b984504b4928df44
Alexandru Badita <abadita@cbn.ro>
2017-06-29 12:11:34 +0300

    added more plugins

---
+++ Data/code/extensions/humao.rest-client-0.14.6/out/src/models/httpResponse.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class HttpResponse {
+    constructor(statusCode, statusMessage, httpVersion, headers, body, elapsedMillionSeconds, requestUrl, bodySizeInBytes, headersSizeInBytes, bodyStream, timingPhases) {
+        this.statusCode = statusCode;
+        this.statusMessage = statusMessage;
+        this.httpVersion = httpVersion;
+        this.headers = headers;
+        this.body = body;
+        this.elapsedMillionSeconds = elapsedMillionSeconds;
+        this.requestUrl = requestUrl;
+        this.bodySizeInBytes = bodySizeInBytes;
+        this.headersSizeInBytes = headersSizeInBytes;
+        this.bodyStream = bodyStream;
+        this.timingPhases = timingPhases;
+    }
+    getResponseHeaderValue(name) {
+        if (this.headers) {
+            for (var header in this.headers) {
+                if (header.toLowerCase() === name.toLowerCase()) {
+                    return this.headers[header];
+                }
+            }
+        }
+        return null;
+    }
+}
+exports.HttpResponse = HttpResponse;
+//# sourceMappingURL=httpResponse.js.map
