c34a697190ffaf73ca972d821402e22738e1b96b
Alexandru Badita <abadita@cbn.ro>
2017-07-12 10:04:06 +0300

    updated Node & added Python

---
+++ Data/code/extensions/donjayamanne.javadebugger-0.1.2/out/src/test/extension.test.js
@@ -0,0 +1,16 @@
+"use strict";
+//
+// Note: This example test is leveraging the Mocha test framework.
+// Please refer to their documentation on https://mochajs.org/ for help.
+//
+Object.defineProperty(exports, "__esModule", { value: true });
+// import * as myExtension from '../src/extension';
+// // Defines a Mocha test suite to group tests of similar kind together
+// suite("Extension Tests", () => {
+//     // Defines a Mocha unit test
+//     test("Something 1", () => {
+//         assert.equal(-1, [1, 2, 3].indexOf(5));
+//         assert.equal(-1, [1, 2, 3].indexOf(0));
+//     });
+// }); 
+//# sourceMappingURL=extension.test.js.map
